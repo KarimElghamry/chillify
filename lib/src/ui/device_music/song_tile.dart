fd94c846c490a7821cbed1a56df79482fff2102b
KarimElghamry <Karimelghamry@gmail.com>
2019-05-26 00:04:34 +0200

    finalized song tile

--- lib/src/ui/device_music/song_tile.dart
+++ lib/src/ui/device_music/song_tile.dart
@@ -7,95 +7,132 @@ import 'package:provider/provider.dart';
 class SongTile extends StatelessWidget {
   final Song _song;
   String _artists;
-  SongTile({@required Song song}) : _song = song;
+  String _duration;
+  SongTile({Key key, @required Song song})
+      : _song = song,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final GlobalBloc _globalBloc = Provider.of<GlobalBloc>(context);
+    parseArtists();
+    parseDuration();
+    return StreamBuilder<MapEntry<PlayerState, Song>>(
+        stream: _globalBloc.musicPlayerBloc.playerState$,
+        builder: (BuildContext context,
+            AsyncSnapshot<MapEntry<PlayerState, Song>> snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
+
+          final PlayerState _state = snapshot.data.key;
+          final Song _currentSong = snapshot.data.value;
+          final bool _isSelectedSong = _song == _currentSong;
+          return Container(
+            height: 90,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: _isSelectedSong
+                  ? LinearGradient(
+                      colors: [
+                        Color(0xFFDDEAF2).withOpacity(0.7),
+                        Colors.white,
+                      ],
+                    )
+                  : LinearGradient(
+                      colors: [
+                        Colors.white,
+                        Colors.white,
+                      ],
+                    ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerLeft,
+                      child: _isSelectedSong && _state == PlayerState.playing
+                          ? PauseIcon()
+                          : PlayIcon(),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 8,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Container(
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              _song.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFF4D6B9C),
+                                fontWeight: FontWeight.w700,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              _artists,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFFAAB7CB),
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        _duration,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF94A6C5),
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  void parseDuration() {
+    final double _temp = _song.duration / 1000;
+    final int _minutes = (_temp / 60).floor();
+    final int _seconds = (((_temp / 60) - _minutes) * 60).round();
+    if (_seconds.toString().length != 1) {
+      _duration = _minutes.toString() + ":" + _seconds.toString();
+    } else {
+      _duration = _minutes.toString() + ":0" + _seconds.toString();
+    }
+  }
+
+  void parseArtists() {
     _artists = _song.artist.split(";").reduce((String a, String b) {
       return a + " & " + b;
     });
-    return Container(
-      height: 90,
-      width: double.infinity,
-      color: Colors.transparent,
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            flex: 2,
-            child: StreamBuilder<MapEntry<PlayerState, Song>>(
-              stream: _globalBloc.musicPlayerBloc.playerState$,
-              builder: (BuildContext context,
-                  AsyncSnapshot<MapEntry<PlayerState, Song>> snapshot) {
-                if (!snapshot.hasData) {
-                  return Container();
-                }
-
-                final PlayerState _state = snapshot.data.key;
-                final Song _currentSong = snapshot.data.value;
-
-                final bool _isSelectedSong = _song == _currentSong;
-                return Center(
-                  child: _isSelectedSong && _state == PlayerState.playing
-                      ? PauseIcon()
-                      : PlayIcon(),
-                );
-              },
-            ),
-          ),
-          Flexible(
-            flex: 8,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Container(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _song.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xFF4D6B9C),
-                        fontWeight: FontWeight.w700,
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(
-                      _artists,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xFFAAB7CB),
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Container(
-              width: double.infinity,
-              child: Text(
-                _song.duration.toString(),
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF94A6C5),
-                ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
 
@@ -109,7 +146,7 @@ class PlayIcon extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.transparent,
         border: Border.all(
-          color: Color(0xFFA9B6C4),
+          color: Color(0xFFA1AFBC),
         ),
         borderRadius: BorderRadius.circular(
           _radius,
@@ -118,6 +155,7 @@ class PlayIcon extends StatelessWidget {
       child: Center(
         child: Icon(
           Icons.play_arrow,
+          color: Color(0xFFA1AFBC),
           size: 32.0,
         ),
       ),
@@ -135,7 +173,7 @@ class PauseIcon extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.transparent,
         border: Border.all(
-          color: Color(0xFFA9B6C4),
+          color: Color(0xFF6D84C1),
         ),
         borderRadius: BorderRadius.circular(
           _radius,
@@ -144,6 +182,7 @@ class PauseIcon extends StatelessWidget {
       child: Center(
         child: Icon(
           Icons.pause,
+          color: Color(0xFF6D84C1),
           size: 32.0,
         ),
       ),
