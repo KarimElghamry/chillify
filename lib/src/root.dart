5fe5c8fdf9fb511f2296d9c4a9c91611c0cf33cf
KarimElghamry <Karimelghamry@gmail.com>
2019-05-25 16:22:48 +0200

    app skeleton

---
+++ lib/src/root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ChillifyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
