87b90dd6f62b06596c663ba7e3fbb638b8442b7a
KarimElghamry <Karimelghamry@gmail.com>
2019-05-25 19:40:19 +0200

    temp screen for tests

---
+++ lib/src/ui/device_music/device_music_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flute_music_player/flute_music_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:music_app/src/blocs/global.dart';
+import 'package:music_app/src/models/playerstate.dart';
+import 'package:provider/provider.dart';
+
+class DeviceMusicScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final GlobalBloc _globalBloc = Provider.of<GlobalBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Device Music",
+          style: TextStyle(
+            color: Colors.blueGrey,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: StreamBuilder<List<Song>>(
+        stream: _globalBloc.musicPlayerBloc.songs$,
+        builder: (BuildContext context, AsyncSnapshot<List<Song>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final List<Song> _songs = snapshot.data;
+
+          return ListView.builder(
+            itemCount: _songs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final String _title = _songs[index].title;
+              final String _uri = _songs[index].uri;
+              return StreamBuilder<MapEntry<PlayerState, Song>>(
+                  stream: _globalBloc.musicPlayerBloc.playerState$,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<MapEntry<PlayerState, Song>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Container();
+                    }
+                    final PlayerState _state = snapshot.data.key;
+                    final Song _currentSong = snapshot.data.value;
+                    final bool _isSelectedSong = _currentSong == _songs[index];
+                    return ListTile(
+                      onTap: () {
+                        switch (_state) {
+                          case PlayerState.playing:
+                            if (_isSelectedSong) {
+                              _globalBloc.musicPlayerBloc
+                                  .pauseMusic(_currentSong);
+                            } else {
+                              _globalBloc.musicPlayerBloc.stopMusic();
+                              _globalBloc.musicPlayerBloc.playMusic(
+                                _songs[index],
+                              );
+                            }
+                            break;
+                          case PlayerState.paused:
+                            if (_isSelectedSong) {
+                              _globalBloc.musicPlayerBloc
+                                  .playMusic(_songs[index]);
+                            } else {
+                              _globalBloc.musicPlayerBloc.stopMusic();
+                              _globalBloc.musicPlayerBloc.playMusic(
+                                _songs[index],
+                              );
+                            }
+                            break;
+                          case PlayerState.stopped:
+                            _globalBloc.musicPlayerBloc
+                                .playMusic(_songs[index]);
+                            break;
+                          default:
+                            break;
+                        }
+                      },
+                      leading: _isSelectedSong && _state == PlayerState.playing
+                          ? Icon(
+                              Icons.pause_circle_outline,
+                            )
+                          : Icon(
+                              Icons.play_circle_outline,
+                            ),
+                      title: Text(_title),
+                    );
+                  });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
