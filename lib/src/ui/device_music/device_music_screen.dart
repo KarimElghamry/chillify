321902714707d5a400d9d5e1b306b8c1ddf41b60
KarimElghamry <Karimelghamry@gmail.com>
2019-05-27 22:14:22 +0200

    moved slide up panel up the widget tree

--- lib/src/ui/device_music/device_music_screen.dart
+++ lib/src/ui/device_music/device_music_screen.dart
@@ -32,69 +32,69 @@ class _DeviceMusicScreenState extends State<DeviceMusicScreen> {
   @override
   Widget build(BuildContext context) {
     final GlobalBloc _globalBloc = Provider.of<GlobalBloc>(context);
-    return Scaffold(
-      appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Container(
-              height: 1,
-              color: Color(0xFFD9EAF1),
-            ),
-          ),
-        ),
-        title: Text(
-          "Device Music",
-          style: TextStyle(
-            color: Color(0xFF274D85),
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-          ),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Icon(
-              Icons.search,
-              size: 32,
-              color: Color(0xFF274D85),
-            ),
-          )
-        ],
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
+    return SlidingUpPanel(
+      controller: _controller,
+      minHeight: 100,
+      maxHeight: MediaQuery.of(context).size.height,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(20.0),
+        topRight: Radius.circular(20.0),
       ),
-      body: SlidingUpPanel(
-        controller: _controller,
-        minHeight: 100,
-        maxHeight: MediaQuery.of(context).size.height,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
+      collapsed: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.0,
+              0.8,
+            ],
+            colors: [
+              Color(0xFF4D90CD),
+              Color(0xFFDF5F9D),
+            ],
+          ),
         ),
-        collapsed: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [
-                0.0,
-                0.8,
-              ],
-              colors: [
-                Color(0xFF4D90CD),
-                Color(0xFFDF5F9D),
-              ],
+        child: BottomPanel(controller: _controller),
+      ),
+      body: Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(1),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                height: 1,
+                color: Color(0xFFD9EAF1),
+              ),
             ),
           ),
-          child: BottomPanel(controller: _controller),
+          title: Text(
+            "Device Music",
+            style: TextStyle(
+              color: Color(0xFF274D85),
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.search,
+                size: 32,
+                color: Color(0xFF274D85),
+              ),
+            )
+          ],
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
         ),
         body: StreamBuilder<List<Song>>(
           stream: _globalBloc.musicPlayerBloc.songs$,
@@ -107,18 +107,12 @@ class _DeviceMusicScreenState extends State<DeviceMusicScreen> {
             final List<Song> _songs = snapshot.data;
 
             return ListView.builder(
+              key: UniqueKey(),
+              padding: const EdgeInsets.only(bottom: 90.0),
               physics: BouncingScrollPhysics(),
-              itemCount: _songs.length + 2,
+              itemCount: _songs.length,
               itemExtent: 90,
               itemBuilder: (BuildContext context, int index) {
-                if (index >= _songs.length) {
-                  return Container(
-                    height: 90,
-                    width: double.infinity,
-                    color: Colors.transparent,
-                  );
-                }
-
                 return StreamBuilder<MapEntry<PlayerState, Song>>(
                   stream: _globalBloc.musicPlayerBloc.playerState$,
                   builder: (BuildContext context,
