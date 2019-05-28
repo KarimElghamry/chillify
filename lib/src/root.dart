7143c80cb8876792456e755eb3523d72b1345faf
KarimElghamry <Karimelghamry@gmail.com>
2019-05-28 22:05:30 +0200

    added permissions bloc to global bloc

--- lib/src/root.dart
+++ lib/src/root.dart
@@ -1,43 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:music_app/src/blocs/global.dart';
-import 'package:music_app/src/blocs/permissions.dart';
 import 'package:music_app/src/ui/music_homepage/music_homepage.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 class ChillifyApp extends StatelessWidget {
-  PermissionsBloc _permissionsBloc = PermissionsBloc();
-
+  final GlobalBloc _globalBloc = GlobalBloc();
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<PermissionStatus>(
-        stream: _permissionsBloc.storagePermissionStatus$,
-        builder:
-            (BuildContext context, AsyncSnapshot<PermissionStatus> snapshot) {
-          if (!snapshot.hasData) {
-            return Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: Colors.white,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-          final PermissionStatus _status = snapshot.data;
-          if (_status == PermissionStatus.denied) {
-            SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-          }
-          return Provider<GlobalBloc>(
-            builder: (BuildContext context) => GlobalBloc(),
-            dispose: (BuildContext context, GlobalBloc value) =>
-                value.dispose(),
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: MusicHomepage(),
-            ),
-          );
-        });
+    return Provider<GlobalBloc>(
+      builder: (BuildContext context) => _globalBloc,
+      dispose: (BuildContext context, GlobalBloc value) => value.dispose(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder<PermissionStatus>(
+          stream: _globalBloc.permissionsBloc.storagePermissionStatus$,
+          builder:
+              (BuildContext context, AsyncSnapshot<PermissionStatus> snapshot) {
+            if (!snapshot.hasData) {
+              return Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.white,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            final PermissionStatus _status = snapshot.data;
+            if (_status == PermissionStatus.denied) {
+              SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+            }
+            _globalBloc.musicPlayerBloc.fetchSongs();
+            return MusicHomepage();
+          },
+        ),
+      ),
+    );
   }
 }
