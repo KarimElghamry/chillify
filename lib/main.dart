21e5ee697b20ffeb552af9fc6ed754d1650f5691
KarimElghamry <Karimelghamry@gmail.com>
2020-03-21 12:14:34 +0200

    app refactor to suit flutter 1.12.13

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/src/root.dart';
 
-void main() => runApp(ChillifyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ChillifyApp());
+}
